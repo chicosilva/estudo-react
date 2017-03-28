d35babe1773ae1d2ea94ac2f4287594ff3868758
chicosilva <chicosilva1@gmail.com>
2017-03-28 19:14:44 -0300

    início do form de cadastro e atualização da listagem

--- src/ReservaLista.js
+++ src/ReservaLista.js
@@ -1,38 +1,109 @@
 import React, { Component } from 'react';
+import $ from "jquery";
+
+var root = 'https://jsonplaceholder.typicode.com';
 
 class ReservaLista extends Component {
   
   constructor(){
     super();
-    
-    this.state = {'lista': [{id:1, nome: 'Francisco', email: 'chicosilva1@gmail.com'}, {id:2, nome: 'Francisco', email: 'chicosilva1@gmail.com'}]}
+    this.state = {'lista': [], name: '', email: '', username: ''}
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setName = this.setName.bind(this);
+    console.log('teste');
   }
   
+  setName(evento){
+    this.setState({name: evento.target.value});
+  }
+
+  componentWillMount(){
+    
+    $.ajax({
+        url: root + '/users/',
+        dataType: 'json',
+        success:function(resposta){
+          this.setState({lista:resposta});
+        }.bind(this)
+    });
+
+  }
+  
+  enviaForm(evento){
+      
+      console.log('form');
+      
+      evento.preventDefault();
+      
+      $.ajax('http://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        data: $("#form_cadastro").serialize(),
+        success: function(resposta){
+            resposta['id'] = Math.floor(Math.random() * 10000);
+            this.state.lista.push(resposta);
+            this.setState({lista:this.state.lista});
+        }.bind(this)
+        
+      });
+
+  }
+
   render() {
-    return (
+    console.log('render');
+    return (<div>
+      
+      <h1>Listagem de Reservas ({this.state.lista.length})</h1>
+      
+      <div>
+        <form id="form_cadastro" onSubmit={this.enviaForm}>
+          
+          <div className="form-group">
+            <label>Nome</label>
+            <input name="name" id="name" className="form-control"/>
+          </div>
+
+          <div className="form-group">
+            <label>e-mail</label>
+            <input name="email" id="email" className="form-control"/>
+          </div>
+
+          <div className="form-group">
+            <label>Usuário</label>
+            <input name="username"  id="username" className="form-control"/>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-success">Enviar</button>
+          </div>
+
+        </form>
+        <hr/>
+      </div>
 
       <table className="table">
                   <thead> 
                     <tr> 
-                    <th></th> <th>First Name</th> 
-                    <th>Last Name</th> <th>Username</th> 
+                    <th></th> 
+                    <th>Nome</th> 
+                    <th>e-mail</th> 
+                    <th>Login</th> 
                     </tr> 
                   </thead> 
                   <tbody> 
-                 {
-            this.state.lista.map(function(cliente){
+                 {this.state.lista.map(function(cliente){
+                  
                   return(
                     <tr key={cliente.id}> 
-                      <th scope="row">{cliente.id}</th> 
-                      <td>Mark</td> 
-                      <td>Otto</td> 
-                      <td>@mdo</td> 
+                      <th>{cliente.id}</th> 
+                      <td>{cliente.name}</td> 
+                      <td>{cliente.email}</td> 
+                      <td>{cliente.username}</td> 
                     </tr>);
 
-        })        
+                  })        
       }
       </tbody> 
-      </table>
+      </table></div>
     );
   }
 }
