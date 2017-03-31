96ebb7c9e20eeb5a37d9d02f191be2f867748d02
chicosilva <chicosilva1@gmail.com>
2017-03-30 22:49:28 -0300

    mensagem de cadastro sucesso

--- src/ReservaLista.js
+++ src/ReservaLista.js
@@ -7,16 +7,20 @@ class ReservaLista extends Component {
   
   constructor(){
     super();
-    this.state = {'lista': [], name: '', email: '', username: ''}
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setName = this.setName.bind(this);
-    console.log('teste');
+    this.state = {
+      'lista': [], 
+      name: '', 
+      email: '', 
+      username: '',
+      message: '',
+      type_message: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    
+    
   }
   
-  setName(evento){
-    this.setState({name: evento.target.value});
-  }
-
   componentWillMount(){
     
     $.ajax({
@@ -29,47 +33,81 @@ class ReservaLista extends Component {
 
   }
   
-  enviaForm(evento){
+  handleSubmit(event){
       
-      console.log('form');
-      
-      evento.preventDefault();
+      event.preventDefault();
       
       $.ajax('http://jsonplaceholder.typicode.com/users', {
         method: 'POST',
         data: $("#form_cadastro").serialize(),
         success: function(resposta){
+            
             resposta['id'] = Math.floor(Math.random() * 10000);
             this.state.lista.push(resposta);
-            this.setState({lista:this.state.lista});
+            this.setState({
+                lista:this.state.lista, 
+                name: '', 
+                email: '', 
+                username: '',
+                message: 'Dados enviado com sucesso!',
+                type_message: 'success'
+              }
+            );
+
         }.bind(this)
         
       });
 
   }
 
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  listagem(){
+
+    return this.state.lista.map(function(cliente){
+                  
+                  return(
+                    <tr key={cliente.id}> 
+                      <th>{cliente.id}</th> 
+                      <td>{cliente.name}</td> 
+                      <td>{cliente.email}</td> 
+                      <td>{cliente.username}</td> 
+                    </tr>);
+
+                  });
+  }
+
   render() {
-    console.log('render');
+      
+    if(this.state.type_message){
+      var classStringName = 'alert alert-' + this.state.type_message;
+      var status = <div className={classStringName}>{this.state.message}</div>;
+    }  
+
     return (<div>
       
       <h1>Listagem de Reservas ({this.state.lista.length})</h1>
       
+      {status}
+
       <div>
-        <form id="form_cadastro" onSubmit={this.enviaForm}>
+        <form id="form_cadastro" onSubmit={this.handleSubmit}>
           
           <div className="form-group">
             <label>Nome</label>
-            <input name="name" id="name" className="form-control"/>
+            <input name="name" id="name" className="form-control" value={this.state.name} onChange={this.handleChange} />
           </div>
-
+          
           <div className="form-group">
             <label>e-mail</label>
-            <input name="email" id="email" className="form-control"/>
+            <input name="email" id="email" className="form-control" value={this.state.email} onChange={this.handleChange}/>
           </div>
 
           <div className="form-group">
             <label>Usuário</label>
-            <input name="username"  id="username" className="form-control"/>
+            <input name="username" id="username" className="form-control" value={this.state.username} onChange={this.handleChange}/>
           </div>
 
           <div className="form-group">
@@ -90,18 +128,7 @@ class ReservaLista extends Component {
                     </tr> 
                   </thead> 
                   <tbody> 
-                 {this.state.lista.map(function(cliente){
-                  
-                  return(
-                    <tr key={cliente.id}> 
-                      <th>{cliente.id}</th> 
-                      <td>{cliente.name}</td> 
-                      <td>{cliente.email}</td> 
-                      <td>{cliente.username}</td> 
-                    </tr>);
-
-                  })        
-      }
+                 {this.listagem()}
       </tbody> 
       </table></div>
     );
